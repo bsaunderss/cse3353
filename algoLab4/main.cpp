e416e42161ca7f504851ea356e888ba1fe532e88
bsaunderss <35714573+bsaunderss@users.noreply.github.com>
2018-11-30 16:35:53 -0600

    Add files via upload

--- algoLab4/main.cpp
+++ algoLab4/main.cpp
@@ -8,11 +8,8 @@
 
 
 #include <iostream>
-#include "organizer.hpp"
 #include "tspRunner.hpp" 
 #include <vector>
-
-
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>
@@ -21,27 +18,14 @@
 using namespace std;
 int main(int argc, const char * argv[]) {
     
-    vector<string> files;
-    vector<string>algoNames;
-    
-    //to control the input files that are being sorted and the sort methods being used, you go to the class 'organizer' and control it from there
-    //right now the data sets with 10 values are being checked and each of the sort methods is being used. You can change this though
-    
-    organizer*c = new organizer ();
-    c->addFilePaths(files);
-    c->addAlgoNames(algoNames);
-    
-   // algorithm * tsp = nullptr;
+    vector<string> files = {"4-nodes.txt", "10-nodes.txt"};
+    vector<string>algoNames = {"dp", "brute", "gentic", "tabu"};
     tspRunner tsp;
-  //  tsp = new tspAlgo();
-   // sort->clearOutput("output_file.txt");//making sure the output file is emtpy
-    
     for(int i = 0; i<files.size(); i++){
         for (int j = 0; j<algoNames.size(); j++){
             tsp.select(algoNames[j]);
             tsp.load(files[i]);
             tsp.execute();
-            
         }
     }
     
